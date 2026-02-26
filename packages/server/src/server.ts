987cb86b4c7c249ae6a45817a61b4b3cd3a68c11
Denis Karagyozov <jsdk9926@gmail.com>
2026-02-26 16:21:06 +0200

    Remove unnecessary packages

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,15 +1,10 @@
 import express, { Application, Request, Response, NextFunction } from "express";
-import session from "express-session";
 import rateLimit from "express-rate-limit";
 import cors from 'cors';
-import connectSessionSequelize from "connect-session-sequelize";
 import dotenv from "dotenv";
 import helmet from "helmet";
-import lusca from "lusca";
 import path from "path";
 import expressLayouts from "express-ejs-layouts";
-
-import sequelize from "../config/database";
 
 dotenv.config();
 
@@ -35,29 +30,6 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const SequelizeStore = connectSessionSequelize(session.Store);
-const sessionStore = new SequelizeStore({
-    db: sequelize,
-});
-
-app.use(
-    session({
-        secret: process.env.SESSION_SECRET as string,
-        store: sessionStore,
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            secure: process.env.NODE_ENV === "production",
-            httpOnly: true,
-            maxAge: Number(process.env.SESSION_MAX_AGE),
-        },
-    }),
-);
-
-app.use(lusca.csrf());
-
-sessionStore.sync();
-
 app.set("views", path.join(__dirname, "../views"));
 
 app.set("view engine", "ejs");
@@ -66,14 +38,7 @@ app.use(expressLayouts);
 
 app.set("layout", "layouts/main");
 
-app.use((req, res) => {
-  res.status(404).render('404', { title: 'Page Not Found' });
-});
-
-app.use((err: any, req: any, res: any, next: any) => {
-  console.error(err.stack);
-  res.status(500).render('500', { title: 'Server Error' });
-});
+//app.use(router);
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     const status = err.status || 500;
