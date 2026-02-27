e13a7a49adcbddd3c93804cbd4d1c49a3f920d17
Denis Karagyozov <jsdk9926@gmail.com>
2026-02-27 16:55:11 +0200

    Add forgotten Organization model id

--- packages/server/models/Organization.ts
+++ packages/server/models/Organization.ts
@@ -7,6 +7,11 @@ class Organization extends Model {}
 
 Organization.init(
     {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
         name: {
             type: DataTypes.STRING,
             allowNull: false,
