85d66c5ecd8f5ffcb6301d1d7d5fe7d41d529337
Elena Karabeteva <e.karabeteva6@gmail.com>
2026-03-27 10:55:42 +0200

    modified organization interfaces file

--- packages/server/src/interfaces/organizationService.interface.ts
+++ packages/server/src/interfaces/organizationService.interface.ts
@@ -2,3 +2,28 @@ export interface MembersFilters {
   role?: string;
   status?: string;
 }
+
+export interface CreateOrganizationData {
+  name: string;
+  description: string;
+  contactEmail: string;
+  locationRegion: string;
+  locationCity: string;
+  websiteUrl?: string;
+}
+
+export interface UpdateOrganizationData {
+  name?: string;
+  description?: string;
+  contactEmail?: string;
+  locationRegion?: string;
+  locationCity?: string;
+  websiteUrl?: string;
+}
+
+export interface SearchOrganizationsFilters {
+  region?: string;
+  city?: string;
+  status?: string;
+  search?: string;
+}
