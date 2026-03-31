533861981011ac932b3f7cb6c194b90a3c445724
Elena Karabeteva <e.karabeteva6@gmail.com>
2026-03-31 11:05:48 +0300

    added organizations endpoints

--- packages/server/src/routes/organizationsRoutes.ts
+++ packages/server/src/routes/organizationsRoutes.ts
@@ -1,8 +1,14 @@
 import express from 'express';
-import { getOrganizationById } from '../controllers/organizationsController';
+import {
+  getOrganizationById,
+  getOrganizationCampaigns,
+  getOrganizations,
+} from '../controllers/organizationsController';
 
 const organizationsRouter = express.Router();
 
+organizationsRouter.get('/', getOrganizations);
 organizationsRouter.get('/:id', getOrganizationById);
+organizationsRouter.get('/:id/campaigns', getOrganizationCampaigns);
 
 export default organizationsRouter;
