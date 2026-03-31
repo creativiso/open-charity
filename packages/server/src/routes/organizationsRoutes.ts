cb2422b9fb09352e31c84f3d5a00b2315b7bdbb8
Elena Karabeteva <e.karabeteva6@gmail.com>
2026-03-31 14:05:11 +0300

    added create and join organization endpoints

--- packages/server/src/routes/organizationsRoutes.ts
+++ packages/server/src/routes/organizationsRoutes.ts
@@ -1,14 +1,23 @@
 import express from 'express';
+
 import {
+  createUserOrganization,
   getOrganizationById,
   getOrganizationCampaigns,
   getOrganizations,
+  joinOrganization,
 } from '../controllers/organizationsController';
+
+import { requireAuth } from '../middleware/auth';
+import { createOrganizationValidation } from '../validators/organizationValidators';
 
 const organizationsRouter = express.Router();
 
 organizationsRouter.get('/', getOrganizations);
 organizationsRouter.get('/:id', getOrganizationById);
 organizationsRouter.get('/:id/campaigns', getOrganizationCampaigns);
+
+organizationsRouter.post('/', createOrganizationValidation, requireAuth, createUserOrganization);
+organizationsRouter.post('/:id/join', requireAuth, joinOrganization);
 
 export default organizationsRouter;
