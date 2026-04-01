5db806127ddd31ea8537da7c0dc23e09cecafa70
Denis Karagyozov <jsdk9926@gmail.com>
2026-04-01 18:23:36 +0300

    Created reject membership endpoint

--- packages/server/src/controllers/api/adminOrgMembersController.ts
+++ packages/server/src/controllers/api/adminOrgMembersController.ts
@@ -2,7 +2,11 @@ import { Request, Response, Router } from 'express';
 
 import { requireAdminJWT, verifyToken } from '../../middleware/jwtAuth';
 
-import { getMemberships } from '../../services/organizationService';
+import {
+  approveMembership,
+  getMemberships,
+  rejectMembership,
+} from '../../services/organizationService';
 import { handleError } from '../../utils';
 
 const adminOrgMembersController = Router();
@@ -21,6 +25,51 @@ adminOrgMembersController.get(
       res.status(200).json(result);
     } catch (err) {
       console.error('Getting pending memberships failed:' + err);
+      handleError(err, res);
+    }
+  }
+);
+
+adminOrgMembersController.patch(
+  '/:id/approve',
+  verifyToken,
+  requireAdminJWT,
+  async (req: Request, res: Response) => {
+    try {
+      const membershipId = req.params.id as string;
+      const adminId = req.user!.id;
+
+      const approvedMembership = await approveMembership(membershipId, adminId);
+
+      res.status(200).json(approvedMembership);
+    } catch (err) {
+      console.error('Could not approve membership:' + err);
+      handleError(err, res);
+    }
+  }
+);
+
+adminOrgMembersController.patch(
+  '/:id/reject',
+  verifyToken,
+  requireAdminJWT,
+  async (req: Request, res: Response) => {
+    try {
+      const membershipId = req.params.id as string;
+      const adminId = req.user!.id;
+
+      const { rejectionReason } = req.body;
+
+      if (!rejectionReason) {
+        res.status(400).json({ error: true, message: 'Rejection reason is required' });
+        return;
+      }
+
+      const rejectedMembership = await rejectMembership(membershipId, adminId, rejectionReason);
+
+      res.status(200).json(rejectedMembership);
+    } catch (err) {
+      console.error('Could not reject membership:' + err);
       handleError(err, res);
     }
   }
