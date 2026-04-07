cd9e6187a55bd7fd9f9f66b4c2dc28202d97f51a
Denis Karagyozov <jsdk9926@gmail.com>
2026-04-07 16:14:07 +0300

    created create category endpoint

--- packages/server/src/controllers/api/adminCategoriesController.ts
+++ packages/server/src/controllers/api/adminCategoriesController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
+
+import { createCategory, getAllCategories } from '../../services/categoryService';
+
 import { handleError } from '../../utils';
-import { getAllCategories } from '../../services/categoryService';
+import { CreateCategoryData } from '../../interfaces/categoryService.interface';
 
 export const getCategories = async (req: Request, res: Response) => {
   try {
@@ -9,6 +12,20 @@ export const getCategories = async (req: Request, res: Response) => {
     res.status(200).json(categories);
   } catch (err) {
     console.error('Could not get categories:' + err);
+    handleError(err, res);
+  }
+};
+
+export const createAdminCategory = async (req: Request, res: Response) => {
+  try {
+    const categoryData: CreateCategoryData = req.body;
+    const adminUserId = req.user!.id;
+
+    const createdCategory = await createCategory(categoryData, adminUserId);
+
+    res.status(201).json(createdCategory);
+  } catch (err) {
+    console.error('Could not create category:' + err);
     handleError(err, res);
   }
 };
--- packages/server/src/routes/adminCategoriesRoutes.ts
+++ packages/server/src/routes/adminCategoriesRoutes.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 
-import { getCategories } from '../controllers/api/adminCategoriesController';
+import { createAdminCategory, getCategories } from '../controllers/api/adminCategoriesController';
 import { requireAdminJWT, verifyToken } from '../middleware/jwtAuth';
+import { createCategoryValidation } from '../validators/categoryValidators';
+import { handleValidationErrors } from '../middleware/handleValidationErrors';
 
 const adminCategoriesRouter = express.Router();
 
 adminCategoriesRouter.get('/', verifyToken, requireAdminJWT, getCategories);
+
+adminCategoriesRouter.post(
+  '/',
+  verifyToken,
+  requireAdminJWT,
+  createCategoryValidation,
+  handleValidationErrors,
+  createAdminCategory
+);
 
 export default adminCategoriesRouter;
